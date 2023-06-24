481d363c35b0800e02f6713e7c3776c2600ce8b5
MrUnikPrince <opuniqueprince@gmail.com>
2023-06-25 00:20:52 +0530

    basics of mongoDb  almost done

--- controllers/Home_controller.js
+++ controllers/Home_controller.js
@@ -1,5 +1,11 @@
+// module.exports.home = function(req, res){
+//     return res.end("<h1> This  is home </h1");
+// }
 module.exports.home = function(req, res){
-    return res.end("<h1> This  is home </h1");
+    // return res.end("<h1> This  is home </h1");
+    return res.render('home', {
+        title: 'Home'
+    });
 }
 module.exports.about = function(req, res){
     return res.end("<h1> This is about </h1");
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');  // npm install express
 const app = express();
 const port = 8000;  // port 
+const expressLayout = require('express-ejs-layouts');  // npm install express-ejs-layouts
 
 
+app.use(expressLayout);
+app.use(express.static('./assets'));
+
+// extract style and script form sub pages into the layout
+app.set('layout extractStyle', true);
+app.set('layout extractScript', true);
 
 // use express router
 app.use('/', require('./routes'));
+
+// setup the view engine
+app.set('view engine', 'ejs');
+app.set('views', './view')
+
 // running the server in a browser
 app.listen(port, function(err){
     if(err){
